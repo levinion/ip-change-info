a23d85be68d2f842c7b88dab02fb3974acf4fe27
levinion <levinion@163.com>
2022-10-24 10:14:50 +0800

    update main.go

--- main.go
+++ main.go
@@ -10,6 +10,18 @@ import (
 	"github.com/jordan-wright/email"
 )
 
+	var (
+		//发件人
+		From = "username <xxx@126.com>"
+		//收件人
+		To = []string{"xxx@163.com"}
+		//邮件标题
+		Subject = ""
+		//服务器地址
+		addr = "smtp.126.com:25"
+		//授权
+		Auth = smtp.PlainAuth("", "xxx@126.com", "授权码", "smtp.126.com")
+	)
 func NewCustomTick(interval int) *time.Ticker {
 	return time.NewTicker(time.Duration(interval) * time.Minute)
 }
@@ -27,11 +39,24 @@ func GetIP() string {
 func SendEmail(oldIP string, newIP string) {
 	content := fmt.Sprintf(`Your IP has been changed from %s to %s`, oldIP, newIP)
 	e := email.NewEmail()
-	e.From = "xxx <xxx@126.com>"
-	e.To = []string{"xxx@163.com"}
-	e.Subject = "此处填写标题"
+	e.From = From
+	e.To = To
+	e.Subject = Subject
 	e.Text = []byte(content)
-	err := e.Send("smtp.126.com:25", smtp.PlainAuth("", "xxx@126.com", "授权码", "smtp.126.com"))
+	err := e.Send(addr, Auth)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func EmailTest(){
+	content := fmt.Sprintln(`Email send successfully`)
+	e := email.NewEmail()
+	e.From = From
+	e.To = To
+	e.Subject = "Your server starting..."
+	e.Text = []byte(content)
+	err := e.Send(addr, Auth)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -46,10 +71,11 @@ func CheckIPChange(oldIP *string) {
 }
 
 func main() {
-	oldIP := ""
-	//调整检查时间间隔,默认每15分钟检查一次
+	oldIP := GetIP()
+	//检查时间间隔,默认每15分钟检查一次
 	t := NewCustomTick(15)
 	log.Println("Server starting...")
+	EmailTest()
 	for {
 		<-t.C
 		CheckIPChange(&oldIP)
